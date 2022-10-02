11d7827c7fac4a042b52df1ed898406791f030f1
Holly <hollykvalheim@gmail.com>
2022-10-02 16:34:20 -0700

    Fix image src, align flex items, and edit js text.

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement('h2');
-h2.textContent = "I added this text with JavaScript. Wow!";
-document.querySelector('footer').appendChild(h2);
+const h3 = document.createElement('h3');
+h3.textContent = "I added this text with JavaScript for some reason";
+document.querySelector('footer').appendChild(h3);

2f621a00851ff7ffbbe0a7c3809668b68bfc16a1
Holly <hollykvalheim@gmail.com>
2022-11-05 20:15:29 -0700

    Finally got events to work.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const h3 = document.createElement('h3');
 h3.textContent = "I added this text with JavaScript for some reason";
-document.querySelector('footer').appendChild('h3');
+console.log('why is this null???:',document.querySelector('footer'))
+document.querySelector('footer').appendChild(h3);
 
 function answer(txt, place) {
-    document.createElement('p');
-    p.textContent = txt
-    document.place.appendChild('p')
+    newp = document.createElement('p');
+    newp.textContent = txt
+    place.appendChild(newp)
 }
 
 const flex1 = document.querySelector('#flexsect1');
-flex1.addEventListener('click', answer('Stinky fish',flex1));
+flex1.addEventListener('click', () => answer('Stinky fish',flex1));
 
 const flex2 = document.querySelector('#flexsect2');
-flex2.addEventListener('click', answer('Fresno, CA',flex2));
+flex2.addEventListener('click', () => answer('Fresno, CA',flex2));
 
 const flex3 = document.querySelector('#flexsect3');
-flex3.addEventListener('click', answer(`Her grandparents' house`,flex3));
+flex3.addEventListener('click', () => answer(`Her grandparents' house`,flex3));
+

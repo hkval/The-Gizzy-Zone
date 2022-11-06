6e16fa14cf5df8734311f90cb1c6711266f2b4b5
Holly <hollykvalheim@gmail.com>
2022-11-05 21:05:47 -0700

    Events only happen once.

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const h3 = document.createElement('h3');
 h3.textContent = "I added this text with JavaScript for some reason";
-console.log('why is this null???:',document.querySelector('footer'))
 document.querySelector('footer').appendChild(h3);
 
 function answer(txt, place) {
     newp = document.createElement('p');
     newp.textContent = txt
+    place.removeChild(place.firstElementChild)
     place.appendChild(newp)
 }
 
 const flex1 = document.querySelector('#flexsect1');
-flex1.addEventListener('click', () => answer('Stinky fish',flex1));
+flex1.addEventListener('click', () => answer('Stinky fish',flex1),
+    {once: true});
 
 const flex2 = document.querySelector('#flexsect2');
-flex2.addEventListener('click', () => answer('Fresno, CA',flex2));
+flex2.addEventListener('click', () => answer('Fresno, CA',flex2),
+    {once: true});
 
 const flex3 = document.querySelector('#flexsect3');
-flex3.addEventListener('click', () => answer(`Her grandparents' house`,flex3));
+flex3.addEventListener('click', () => answer(`Her grandparents' house`,flex3),
+    {once: true});
 
